f6ec79448949c8ad6d8c2f475c079ea1b9bdf215
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-04-24 02:32:31 -0300

    Aula 2 - arquivos estáticos

---
+++ 2-arquivos-estaticos/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client')))
+
+
+app.listen(4000, () => console.log(`Server Running on Port: 4000`));
