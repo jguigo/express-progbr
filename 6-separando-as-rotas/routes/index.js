922f0b33f7d994f8dcca3ea2b91fc6908a89d23f
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-04-27 22:53:57 -0300

    aula6: separando as rotas

--- 6-separando-as-rotas/routes/index.js
+++ 6-separando-as-rotas/routes/index.js
@@ -1,3 +1,41 @@
 const express = require('express');
 const routes = express.Router();
 
+let alunos = [
+   {
+      id: 0,
+      nome: "Jose"
+   },
+   {
+      id: 1,
+      nome: "Maria"
+   },
+   {
+      id: 2,
+      nome: "Joao"
+   },
+   {
+      id: 3,
+      nome: "Marcos"
+   }
+]
+
+routes.get("/", (req, res) => {
+   res.send("Hello World!");
+});
+
+routes.get("/alunos", (req, res)=>{
+
+   res.json(JSON.stringify(aluno));
+});
+
+routes.get("/aluno", (req,res) => {
+   const { id } = req.query;
+   res.json(alunos[id]);
+});
+routes.get("/aluno/:id", (req,res) => {
+   const { id } = req.params;
+   res.json(alunos[id]);
+});
+
+module.exports = routes;
